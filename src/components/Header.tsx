7abb2b3aaeb9d33e7511e883cc6056f132f1dd30
SHUJA <106554018+Shujaulislam@users.noreply.github.com>
2024-08-27 01:47:15 +0530

    advanced navbar added

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import NavbarDemo from './example/navbar-menu-demo'
 
 function Header() {
   return (
-    <header className="bg-white shadow-sm">
+    <>
+    <NavbarDemo />
+    {/* <header className="bg-white shadow-sm">
           <nav className="container mx-auto px-4 py-2 flex justify-between items-center">
             <div className="flex space-x-4">
               {['Car Types', 'Bike Types', 'Tyre Pressure', 'Commercial Tyres', 'Accessories', 'More'].map((item) => (
@@ -11,7 +14,8 @@ function Header() {
             </div>
             <button className="bg-blue-500 text-white px-4 py-2 rounded">Login</button>
           </nav>
-        </header>
+        </header> */}
+        </>
   )
 }
 
